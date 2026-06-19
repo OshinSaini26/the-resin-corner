ab6aadfd813a88d32449289011e325bd345d6117
OshinSaini26 <saini.oshin7295@gmail.com>
2026-06-19 15:28:35 +0530

    Move frontend into backend

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,26 @@
 const express = require('express');
 const cors = require('cors');
-const db = require('./config/db'); 
+const path = require('path');
+const db = require('./config/db');
 
 const app = express();
-//const PORT = 3000;
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'frontend')));
 
 const orderRoutes = require('./routes/orders.routes');
 const userRoutes = require('./routes/users.routes');
+
 app.use('/api/orders', orderRoutes);
 app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => {
-  res.send('Resin Order Platform API is running');
+  res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
 });
-
-/*app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});*/
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
-
